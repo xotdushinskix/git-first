778a2f798b9aa9e8a50a204412ce3d18f6ec5689
Nikita Otdushinski <crimeaxxxattack@gmail.com>
2015-05-26 20:41:09 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -3,7 +3,10 @@ public class Calculate {
 	public static void main(String[] args){
 		
 		System.out.println("Calculate!");
-		
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[0]);
+		int sum = first + second;
+		System.out.println("Sum" + sum);
 	}
 	
 }
